23a6975b19ebce9bd76359ee96fd9e4906ca1da5
ssmold <molchanov.sd@phystech.edu>
2022-10-23 19:08:50 +0300

    add more write implementations

--- 02-fuse-helloworld/solution.c
+++ 02-fuse-helloworld/solution.c
@@ -32,7 +32,8 @@ static int getattr_impl(const char *path, struct stat *st,
 
 static int readdir_impl(const char *path, void *buffer, fuse_fill_dir_t filler,
 		off_t offset __attribute__((unused)),
-		struct fuse_file_info *fi __attribute__((unused)), enum fuse_readdir_flags flags __attribute__((unused)) ) {
+		struct fuse_file_info *fi __attribute__((unused)),
+		enum fuse_readdir_flags flags __attribute__((unused))) {
 
 	filler(buffer, ".", NULL, 0, FUSE_FILL_DIR_PLUS);
 	filler(buffer, "..", NULL, 0, FUSE_FILL_DIR_PLUS);
@@ -67,8 +68,18 @@ static int write_impl(const char *path __attribute__((unused)),
 	return -EROFS;
 }
 
+static int mknod_impl(const char* name __attribute__((unused)),
+		mode_t mode __attribute__((unused)), dev_t dev __attribute__((unused))) {
+	return -EROFS;
+}
+
+static int mkdir_impl(const char * path __attribute__((unused)), mode_t mode __attribute__((unused))) {
+	return -EROFS;
+}
+
 static const struct fuse_operations hellofs_ops = { .getattr = getattr_impl,
-		.readdir = readdir_impl, .read = read_impl, .write = write_impl };
+		.readdir = readdir_impl, .read = read_impl, .write = write_impl,
+		.mknod = mknod_impl, .mkdir = mkdir_impl, };
 
 int helloworld(const char *mntp) {
 	char *argv[] = { "exercise", "-f", (char*) mntp, NULL };
