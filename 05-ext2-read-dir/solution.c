a0051d601404385d01cf628d46caa3d4c0680a88
ssergomol <molchanov.sd@phystech.edu>
2022-11-06 23:02:52 +0300

    small fix

--- 05-ext2-read-dir/solution.c
+++ 05-ext2-read-dir/solution.c
@@ -52,7 +52,7 @@ int read_direct_blocks(unsigned int i_block, int img) {
 
         report_file(inode, type, file_name);
 
-         Move to the next entry
+        // Move to the next entry
         entry = (void *) entry + entry->rec_len;
         size += entry->rec_len;
     }
