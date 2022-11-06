b837ed06ecb222153ba2a4603c5d7877d480aef2
ssergomol <molchanov.sd@phystech.edu>
2022-11-06 15:15:55 +0300

    read ext2 image

--- 04-ext2-read-file/solution.c
+++ 04-ext2-read-file/solution.c
@@ -1,4 +1,8 @@
 #include <solution.h>
+#include <linux/ext2_fs.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
 
 int dump_file(int img, int inode_nr, int out)
 {
@@ -6,6 +10,21 @@ int dump_file(int img, int inode_nr, int out)
 	(void) inode_nr;
 	(void) out;
 
-	/* implement me */
+    struct ext2_super_block super;
+    pread(img, &super, sizeof(super), 1024)
+
+    // Check if the file is an ext2 image
+    if (super.s_magic != EXT2_SUPER_MAGIC) {
+        return -errno;
+    }
+
+    // Get block size in bytes
+    unsigned int block_size = 1024 << super.s_log_block;
+
+
+
+
+
+
 	return 0;
 }
