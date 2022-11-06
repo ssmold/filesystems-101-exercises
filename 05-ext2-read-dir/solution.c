3e8a0dbc396a5e8595d9f54b421ff49091d33f07
ssergomol <molchanov.sd@phystech.edu>
2022-11-06 23:10:11 +0300

    small fix

--- 05-ext2-read-dir/solution.c
+++ 05-ext2-read-dir/solution.c
@@ -34,7 +34,7 @@ int read_direct_blocks(unsigned int i_block, int img) {
         // Get file name
         char file_name[EXT2_NAME_LEN + 1];
         memcpy(file_name, entry->name, entry->name_len);
-        file_name[entry->name_len] = '\0';
+        file_name[entry->name_len] = 0;
 
         // Get file type
 //        unsigned int file_type = entry->file_type;
