43ba58d7c37b548bd6f9595c2a7738ebdfb631ee
ssergomol <molchanov.sd@phystech.edu>
2022-11-06 16:35:19 +0300

    get group desc

--- 04-ext2-read-file/solution.c
+++ 04-ext2-read-file/solution.c
@@ -5,14 +5,18 @@
 #include <unistd.h>
 #include <ext2fs/ext2fs.h>
 
-int dump_file(int img, int inode_nr, int out)
-{
-	(void) img;
-	(void) inode_nr;
-	(void) out;
+#define BOOT_BLOCK_SIZE 1024
+
+int dump_file(int img, int inode_nr, int out) {
+    (void) img;
+    (void) inode_nr;
+    (void) out;
 
     struct ext2_super_block super;
-    pread(img, &super, sizeof(super), 1024);
+    int ret = pread(img, &super, sizeof(super), BOOT_BLOCK_SIZE);
+    if (ret < 0) {
+        return -errno;
+    }
 
     // Check if the file is an ext2 image
     if (super.s_magic != EXT2_SUPER_MAGIC) {
@@ -20,13 +24,18 @@ int dump_file(int img, int inode_nr, int out)
     }
 
     // Get block size in bytes
-    unsigned int block_size = EXT2_MIN_BLOCK_SIZE << super.s_log_block_size;
+    unsigned int BLOCK_SIZE = EXT2_MIN_BLOCK_SIZE << super.s_log_block_size;
 
+    struct ext2_group_desc group_desc;
+    unsigned int group_count = 1 + (super.s_blocks_count - 1) / super.s_blocks_per_group;
+    unsigned int des_list_size = group_count * sizeof(struct ext2_group_descr);
+    unsigned int group_desc_number = (inode_nr - 1) / super.s_inodes_per_group;
 
+    ret = pread(img, &group_desc, sizeof(group_desc),
+          BOOT_BLOCK_SIZE + BLOCK_SIZE + group_desc_number * sizeof(ext2_group_desc);
+    if (ret < 0) {
+        return -errno;
+    }
 
-
-
-
-
-	return 0;
+    return 0;
 }
