ef10957f7afb35a256e2fe41ed549afa9552110c
ssergomol <molchanov.sd@phystech.edu>
2022-11-13 20:19:11 +0300

    fix types prob

--- 06-ext2-walk-path/solution.c
+++ 06-ext2-walk-path/solution.c
@@ -274,7 +274,7 @@ char* get_next_dir_name(const char* ptr, unsigned* length) {
     char* endPtr = strpbrk(ptr + 1, "/");
     *length = endPtr - ptr - 1;
     const char* dirName = ptr + 1;
-    strncpy(name, dirName, length);
+    strncpy(name, dirName, *length);
     return endPtr;
 }
 
