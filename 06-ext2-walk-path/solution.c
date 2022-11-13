288d115fcf99b75022708796b5e2a254d44d07fc
ssergomol <molchanov.sd@phystech.edu>
2022-11-13 19:04:12 +0300

    get each dir name

--- 06-ext2-walk-path/solution.c
+++ 06-ext2-walk-path/solution.c
@@ -1,11 +1,34 @@
 #include <solution.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int dump_file(int img, const char *path, int out)
-{
+
+char* get_next_dir_name(const char* ptr, unsigned* length, const char** name) {
+    char* endPtr = strpbrk(ptr + 1, "/");
+    *length = endPtr - ptr - 1;
+    const char* dirName = ptr + 1;
+    *name = dirName;
+    return endPtr;
+}
+
+
+int dump_file(int img, const char *path, int out) {
 	(void) img;
 	(void) path;
 	(void) out;
 
-	/* implement me */
-	return 0;
+    const char* charPtr = path;
+    const char** dirName;
+    unsigned length = 0;
+    char* name = (char *)malloc(sizeof(char) * 256);
+
+    while ((charPtr = get_next_dir_name(charPtr, &length, dirName))) {
+        stpncpy(name, *dirName, length);
+        name[length] = '\0';
+
+    }
+
+    free(name);
+    return 0;
 }
