f653c544078febc2059ea3fd459b567c3c477b55
ssergomol <molchanov.sd@phystech.edu>
2022-12-11 22:16:12 +0300

    solve task 11

---
+++ 11-grpc/pkg/parhash/server.go
@@ -0,0 +1,157 @@
+package parhash
+
+import (
+	"context"
+	"net"
+	"sync"
+	"sync/atomic"
+
+	hashpb "fs101ex/pkg/gen/hashsvc"
+	pb "fs101ex/pkg/gen/parhashsvc"
+	"fs101ex/pkg/workgroup"
+
+	"github.com/pkg/errors"
+	"golang.org/x/sync/semaphore"
+	"google.golang.org/grpc"
+)
+
+type roundRobin struct {
+	backends []hashpb.HashSvcClient
+	next     uint32
+}
+
+func NewRoundRobin(clients []hashpb.HashSvcClient) (*roundRobin, error) {
+	return &roundRobin{
+		backends: clients,
+	}, nil
+}
+
+func (r *roundRobin) Next() hashpb.HashSvcClient {
+	current := atomic.AddUint32(&r.next, 1) - 1
+	return r.backends[int(current)%len(r.backends)]
+}
+
+type Config struct {
+	ListenAddr   string
+	BackendAddrs []string
+	Concurrency  int
+}
+
+// Implement a server that responds to ParallelHash()
+// as declared in /proto/parhash.proto.
+//
+// The implementation of ParallelHash() must not hash the content
+// of buffers on its own. Instead, it must send buffers to backends
+// to compute hashes. Buffers must be fanned out to backends in the
+// round-robin fashion.
+//
+// For example, suppose that 2 backends are configured and ParallelHash()
+// is called to compute hashes of 5 buffers. In this case it may assign
+// buffers to backends in this way:
+//
+//	backend 0: buffers 0, 2, and 4,
+//	backend 1: buffers 1 and 3.
+//
+// Requests to hash individual buffers must be issued concurrently.
+// Goroutines that issue them must run within /pkg/workgroup/Wg. The
+// concurrency within workgroups must be limited by Server.sem.
+//
+// WARNING: requests to ParallelHash() may be concurrent, too.
+// Make sure that the round-robin fanout works in that case too,
+// and evenly distributes the load across backends.
+type Server struct {
+	conf Config
+
+	m    sync.Mutex
+	sem  *semaphore.Weighted
+	stop context.CancelFunc
+	l    net.Listener
+	wg   sync.WaitGroup
+}
+
+func New(conf Config) *Server {
+	return &Server{
+		conf: conf,
+		sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
+	}
+}
+
+func (s *Server) Start(ctx context.Context) (err error) {
+	defer func() { err = errors.Wrap(err, "Start()") }()
+
+	ctx, s.stop = context.WithCancel(ctx)
+
+	s.l, err = net.Listen("tcp", s.conf.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	srv := grpc.NewServer()
+	pb.RegisterParallelHashSvcServer(srv, s)
+
+	s.wg.Add(2)
+	go func() {
+		defer s.wg.Done()
+
+		srv.Serve(s.l)
+	}()
+	go func() {
+		defer s.wg.Done()
+
+		<-ctx.Done()
+		s.l.Close()
+	}()
+
+	return nil
+}
+
+func (s *Server) ListenAddr() string {
+	return s.l.Addr().String()
+}
+
+func (s *Server) Stop() {
+	s.stop()
+	s.wg.Wait()
+}
+
+func (s *Server) ParallelHash(ctx context.Context, req *pb.ParHashReq) (res *pb.ParHashResp, err error) {
+	wg := workgroup.New(workgroup.Config{Sem: s.sem})
+	hashes := make([][]byte, len(req.Data))
+	conns := make([]*grpc.ClientConn, len(s.conf.BackendAddrs))
+	clients := make([]hashpb.HashSvcClient, len(s.conf.BackendAddrs))
+
+	for i := range clients {
+		if conns[i], err = grpc.Dial(s.conf.BackendAddrs[i]); err != nil {
+			return &pb.ParHashResp{}, err
+		}
+		defer conns[i].Close()
+
+		clients[i] = hashpb.NewHashSvcClient(conns[i])
+	}
+
+	r, err := NewRoundRobin(clients)
+	if r != nil {
+		return &pb.ParHashResp{}, err
+	}
+
+	for i, bytes := range req.Data {
+		wg.Go(ctx, func(ctx context.Context) error {
+
+			hashReq := &hashpb.HashReq{Data: bytes}
+			res, err := r.Next().Hash(ctx, hashReq)
+			if err != nil {
+				return err
+			}
+
+			s.m.Lock()
+			hashes[i] = res.Hash
+			s.m.Unlock()
+			return nil
+		})
+	}
+
+	if err := wg.Wait(); err != nil {
+		return &pb.ParHashResp{}, err
+	}
+	return &pb.ParHashResp{Hashes: hashes}, nil
+}
